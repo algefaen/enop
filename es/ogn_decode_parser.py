192f9f17690b057d494e4b0af3543e5894210437
Christian Grovdal <chrgro@gmail.com>
2016-08-23 22:11:03 +0200

    Added time based index names
    Defaults to weeks, in the format yyyy-wWW

--- es/ogn_decode_parser.py
+++ es/ogn_decode_parser.py
@@ -12,6 +12,7 @@ import argparse
 # Global configuration:
 # Elastic type name, mostly don't care so naming it loc
 elastic_type = "loc"
+elastic_index = "enop_flarm"
 
 
 def main():
@@ -19,7 +20,7 @@ def main():
 
 	parser = argparse.ArgumentParser(description="Parse OGN decode stream and upload to ES")
 	parser.add_argument("--dry-run", action="store_true", help="Do not upload")
-	parser.add_argument("--input-file", help="Read OGN decode data from file instead of stdin")
+	parser.add_argument("--input-file", help="Read OGN decode data from file instead of stdin. Note that this will timestamp all entries with current time.")
 	parser.add_argument("--no-pass-through", help="Do not print out the decode stream during decoding", action="store_true")
 	parser.add_argument("--es-server", help="URL:port to elasticsearch", default="localhost:9200")
 	args = parser.parse_args()
@@ -30,18 +31,25 @@ def main():
 
 
 	# Attempt to write a typemapping first
-	typemapping = json.dumps({
+	typemapping = json.dumps(
+	{
+		"template" : "enop_flarm*",
 		"mappings" : {
 			elastic_type : {
 				"properties" : {
 					"position" : {"type":"geo_point"}
 				}
 			}
-		}})
+		}
+	})
 
 
-	#print typemapping
-	es.indices.create(index="enop_flarm", body=typemapping, ignore=400)
+	# For single index:
+	#es.indices.create(index="enop_flarm", body=typemapping, ignore=400)
+	# For timestamped index add a template:
+	es.indices.put_template(name="enop_template", body=typemapping)
+
+	#exit(0)
 
 	#
 	# Keep this alive and waiting for lines
@@ -54,7 +62,6 @@ def main():
 		while True:
 			line = sys.stdin.readline()
 			parseline(line)
-	exit(0)
 
 def parseline(line):
 	global es, args
@@ -82,11 +89,18 @@ def parseline(line):
 	data["distance"] = float(fieldsre.group(15))
 	data["bearing"] = float(fieldsre.group(16))
 
-	data["timestamp"] = datetime.datetime.now().isoformat()
+	rightnow = datetime.datetime.now()
+	data["timestamp"] = rightnow.isoformat()
+
+	# Month based indexing:
+	#timeindex = "enop_flarm-"+str(rightnow.year)+"-"+str(rightnow.month)
+	# Week based indexing:
+	(year,week, dontcare) = rightnow.isocalendar()
+	timeindex = "enop_flarm-"+str(year)+"-w"+str(week)
 
 	# Ship the data!
 	if not args.dry_run:
-		es.index(index="enop_flarm", doc_type="loc", body=json.dumps(data))
+		es.index(index=timeindex, doc_type=elastic_type, body=json.dumps(data))
 
 	print "** UPLOADED TO ES: **", data
 
